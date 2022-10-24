de98ec8ef8788860566e1ca7581b9d0f24639a64
Sagnik <csagnik13@gmail.com>
2022-10-25 01:36:13 +0530

    Added several features to the Original Game

--- script.js
+++ script.js
@@ -2,19 +2,14 @@ function playRound(playerSelection,computerSelection){
     computerSelection=computerSelection.toLowerCase();
     playerSelection=playerSelection.toLowerCase();
     if (playerSelection==computerSelection){
-        console.log("It's a draw!");
         return 0;
     }else if (playerSelection=="rock" && computerSelection=="scissors"){
-        console.log(`You win this Round! ${playerSelection} beats ${computerSelection}`);
         return 1;
     }else if (playerSelection=='paper' && computerSelection=='rock'){
-        console.log(`You win this Round! ${playerSelection} beats ${computerSelection}`);
         return 1;
     }else if (playerSelection=='scissors' && computerSelection=='paper'){
-        console.log(`You win this Round! ${playerSelection} beats ${computerSelection}`);
         return 1;
     }else{
-        console.log(`You Lose this Round! ${computerSelection} beats ${playerSelection}`);
         return -1;
     }
 }
@@ -24,19 +19,32 @@ function getComputerChoice(){
     else if (randomNumber<=60) return "Paper";
     else return "Scissors";
 }
-function game(){
-    let score=0;
-    for(let i=0;i<5;i++){
-        let computerChoice=getComputerChoice();
-        let playerChoice=prompt('Enter Rock, Paper or Scissors...');
-        score+=playRound(playerChoice,computerChoice);
+const res=document.querySelector('.Result');
+let playerScore=0, compScore=0;
+function game(update){
+    if (update>0){
+        playerScore+=1;
+    }else if (update<0){
+        compScore+=1;
     }
-    if (score>0){
-        console.log("You win the Best Of 5.");
-    }else if (score<0){
-        console.log("Computer Wins the Best Of 5.");
-    }else{
-        console.log("It's a draw!");
+    res.textContent=`Player Score: ${playerScore}  Computer Score: ${compScore}`;
+    if (playerScore==5 || compScore==5){
+        if (playerScore==5){
+            alert('You Win!');
+        }else{
+            alert('The Computer Wins!');
+        }
+        playerScore=0;
+        compScore=0;
+        res.textContent=`Player Score: 0  Computer Score: 0`;
     }
 }
-game();
+function playTheGame(btn){
+    btn.addEventListener('click',(e)=>{
+        const btnName=e.target.id;
+        let k=playRound(btnName,getComputerChoice());
+        game(k);
+    });
+}
+const btns=document.querySelectorAll('button');
+btns.forEach(playTheGame);
